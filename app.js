ec40c5b4fbbd286e3e3a4dca46290c6aff602d37
fabcodingzest <fabrizvi786@gmail.com>
2020-07-10 15:49:21 +0530

    Dynamic data with ejs

--- app.js
+++ app.js
@@ -11,15 +11,20 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
   //res.send('<p> home page </p>')
-  res.render('index')
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', {title: 'Home', blogs})
 })
 app.get('/about', (req, res) => {
   //res.send('<p> about page </p>')
-  res.render('about')
+  res.render('about' , {title: 'About'})
 })
 
 app.get('/blogs/create', (req, res) => {
-  res.render('create')
+  res.render('create', {title: 'Create'})
 })
 
 // redirect
@@ -29,5 +34,5 @@ app.get('/blogs/create', (req, res) => {
 
 // 404 page (needs to be at bottom of routes)
 app.use((req, res) => {
-  res.status(404).render('404')
+  res.status(404).render('404', {title: 'Page Not Found'})
 })

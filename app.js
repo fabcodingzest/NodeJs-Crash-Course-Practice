def8a339ba765e95788b2b66e824a61c070e0bef
fabcodingzest <fabrizvi786@gmail.com>
2020-07-12 01:02:30 +0530

    added diff reqs, Express Router and MVC structure

--- app.js
+++ app.js
@@ -1,37 +1,71 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 //express app
 const app = express();
 
+const dbURI = 'mongodb+srv://fab-blogger:Iamabloggerxoxo@cluster0.8q6uk.mongodb.net/fab-blogger?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err))
 // register view engine
 app.set('view engine', 'ejs')
 
 // listen for requests
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'))
-
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'));
+
+
+
+//mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+//   const blog = new Blog({
+//     title: 'Fab blog number 2',
+//     snippet: 'about the fucking blog',
+//     body: 'moreeeeeeeeeeeeeeeeee about blog'
+//   });
+
+//   blog.save()
+//     .then((result) => {
+//     res.send(result)
+//     })
+//     .catch((err) => {
+//     console.log(err);
+//   })
+// })
+
+// app.get('/all-blogs', (req, res) => {
+//   Blog.find()
+//       .then(result => {
+//     res.send(result)
+//     })
+//     .catch((err) => {console.log(err);})
+// })
+
+// app.get('/single-blog', (req, res) => {
+//   Blog.findById('5f09ac76c4b5a06354a57f53')
+//     .then(result => {
+//     res.send(result)
+//     })
+//     .catch((err) => {console.log(err);})
+// })
 
 app.get('/', (req, res) => {
   //res.send('<p> home page </p>')
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', {title: 'Home', blogs})
+  res.redirect('/blogs')
 })
 app.get('/about', (req, res) => {
   //res.send('<p> about page </p>')
   res.render('about' , {title: 'About'})
 })
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', {title: 'Create'})
-})
+// blog routes
+app.use('/blogs', blogRoutes);
 
 // redirect
 // app.get('/about-us', (req, res) => {
